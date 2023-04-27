74445d741741ddba2376bcd78bed4e14c8954fb2
zen <12.zeinirfansyah@gmail.com>
2023-04-27 19:13:13 +0700

    ubah total

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,65 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
 import foto from "../../assets/images/foto.jpg";
-import pixel_totoro1 from "../../assets/images/pixel_totoro1.png";
-import discordicon from "../../assets/images/icons8-discord-50-7.svg";
-import instagramicon from "../../assets/images/icons8-instagram-50.svg";
+import discordicon from "../../assets/images/discordicon.svg";
+import githubicon from "../../assets/images/githubicon.svg";
+import instagramicon from "../../assets/images/instagramicon.svg";
+import brimstone from "../../assets/images/brimstone.png";
+import jett from "../../assets/images/jett.png";
 
 const Hero = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <div className="Hero">
+      <img src={brimstone} alt="" className="brimstone" />
+      <img src={jett} alt="" className="jett" />
       <div className="container">
-        <div className="card w-75 ms-auto">
-          <div className="row">
-            <h2 className="title-card text-center">Zeemarimo Card</h2>{" "}
-            <hr className="line " />
-            <div className="col-3">
-              <div className="hero__img">
-                <img className="foto foto1" src={foto} alt="" />
+        <div className={`flip-card ${isFlipped ? "flipped" : ""}`}>
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <h2 className="title">ZEEMARIMO</h2>
+              <div className="desc">
+                <p>
+                  Introducing Zee, codenamed <span>zeemarimo</span>, the newest
+                  Valorant Agent from <span>Indonesia</span> specializing in
+                  software development. Zee is a junior software engineer with a
+                  passion for creating functional technology. He is a highly
+                  skilled agent with a diverse skill set and a keen eye for
+                  detail.
+                </p>
+                <p>
+                  {" "}
+                  Zee's expertise in coding allows him to work with any
+                  programming language and his artistic skills make him an
+                  exceptional Frontend engineer. His signature ability is{" "}
+                  <span>Frontend Web Development</span>, which allows him to
+                  create stunning and functional user interfaces for websites.
+                  Additionally, his ultimate ability is{" "}
+                  <span>Full-stack Web Development</span>, enabling him to build
+                  complete and dynamic web applications.
+                </p>
+                <p>
+                  With his diverse skill set and attention to detail, he is a
+                  valuable asset to any team looking for a highly skilled agent.
+                </p>
               </div>
             </div>
-            <div className="col">
-              <div className="hero__text w-75">
-                <div className="row">
-                  <div className="col text">
-                    <p>
-                      Hai I'm <b>zeemarimo</b>, you can call me <b>zee</b>. I'm
-                      a frontend engineer and I love to create things. I'm
-                      currently live in Yogyakarta, Indonesia. I'm a self-taught
-                      programmer and I love to learn new things.
-                    </p>
-                    <p>
-                      {" "}
-                      I have a passion in art especially sketch art. I love to
-                      play games even though I'm not good at it.
-                    </p>
-                    <p>
-                      <b>Nice to meet you :D</b>
-                    </p>
-                  </div>
-                </div>
-              </div>
+            <div className="flip-card-back">
+              <h2 className="title">ZEEMARIMO</h2>
               <div className="row">
-                <div className="col socmed">
+                <div className="col">
+                  <div className="row">
+                    <div className="col">
+                      <table class=" m-3 table table-hover table-dark table-striped text-start">
+                        <tbody>
+                          <tr>
+                            <td>REAL NAME</td>
+                            <td>Zein Irfansyah</td>
+                          </tr>
+                          <tr>
+                            <td>CODENAME</td>
+                            <td>zeemarimo</td>
+                          </tr>
+                          <tr>
+                            <td>ORIGIN</td>
+                            <td>Indonesia</td>
+                          </tr>
+                          <tr>
+                            <td>RACE</td>
+                            <td>Sunda</td>
+                          </tr>
+                          <tr>
+                            <td>GENDER</td>
+                            <td>Male</td>
+                          </tr>
+                          <tr>
+                            <td>ROLE</td>
+                            <td>Engineer</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
                   <div className="row">
                     <div className="col">
                       <a href="https://dsc.gg/mindmesh" target="_blank">
-                        <img src={discordicon} alt="" className="discord" />
-                        zee #1790
+                        <img
+                          src={discordicon}
+                          alt=""
+                          className="icon discord"
+                        />{" "}
                       </a>
                     </div>
                     <div className="col">
-                      <a href="https://instagram.com/zeemarimo" target="_blank">
-                        <img src={instagramicon} alt="" />
-                        zeemarimo
+                      <a
+                        href="https://github.com/zeinirfansyah"
+                        target="_blank"
+                      >
+                        <img src={githubicon} alt="" className="icon github" />
+                      </a>
+                    </div>
+                    <div className="col">
+                      <a
+                        href="https://www.instagram.com/zeemarimo/"
+                        target="_blank"
+                      >
+                        <img
+                          src={instagramicon}
+                          alt=""
+                          className="icon instagram"
+                        />
                       </a>
                     </div>
                   </div>
+                </div>
+                <div className="col-4">
+                  <img src={foto} alt="" className="foto" />
                 </div>
               </div>
             </div>
           </div>
+          <div className="clicktoflip text-end mt-2" onClick={handleFlip}>
+            <span>Flip card</span>
+            <hr />
+          </div>
+          <div className="catatan">
+            <p>
+              Note: For now this website doesn't support mobile devices. Make
+              sure you open it in your desktop browser.{" "}
+            </p>{" "}
+            <p>© 2023 Zein Irfansyah. All rights reserved.</p>
+          </div>
         </div>
-        <img className="imgassets imgasset3" src={pixel_totoro1} alt="" />
       </div>
     </div>
   );
