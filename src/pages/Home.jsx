abc2d628b34f79316c1e5b4cca9c04421a8edc69
zen <12.zeinirfansyah@gmail.com>
2023-05-13 01:13:11 +0700

    ubah mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,12 @@ const Home = () => {
   return (
     <div className="Home">
       <Hero />
+      <div className="mobile">
+        <p className="text-center text-dark mt-5 fs-3 p-3">
+          Sorry, for now this website doesn't support mobile devices. Make sure
+          you open it in your desktop browser. -zee
+        </p>
+      </div>
     </div>
   );
 };
